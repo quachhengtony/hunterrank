8dc6e0076bbe96c561b587cecf0cb0a5e0dd4b44
TonyInfinity <quachhengtony@gmail.com>
2022-06-07 23:49:23 +0700

    Fix auth module dependency errors

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
 import { AuthController } from './auth.controller';
+import { AuthService } from './auth.service';
 
 @Module({
+  imports: [JwtModule.register({})],
   controllers: [AuthController],
-  providers: [AuthModule],
-  exports: [AuthModule],
+  providers: [AuthService],
 })
 export class AuthModule {}
--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,6 +1,7 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 
+@Global()
 @Module({
   providers: [ConfigService],
   exports: [ConfigService],
